db5b43f29822f63c15749748e9e79f7d3e93512f
zhangyong0010 <etngod@gmail.com>
2026-08-19 21:14:43 +0800

    发布 v30：导出按钮与导航布局优化

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='minchao-v29';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./style.css?v=29','./data.js?v=29','./more-data.js?v=29','./app.js?v=29'];
+const CACHE='minchao-v30';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./style.css?v=30','./data.js?v=30','./more-data.js?v=30','./app.js?v=30'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
