ae41e4d56846687a2523f55f769183d241a1b6a8
zhangyong0010 <etngod@gmail.com>
2026-08-19 20:09:47 +0800

    发布行程设置自动保存与阅读优化

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='minchao-v24';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./style.css?v=24','./data.js?v=24','./more-data.js?v=24','./app.js?v=24'];
+const CACHE='minchao-v25';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./style.css?v=25','./data.js?v=25','./more-data.js?v=25','./app.js?v=25'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
