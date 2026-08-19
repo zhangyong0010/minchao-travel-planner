1c62a70b87217e42d482efb38d597c7a2d2b18cd
zhangyong0010 <etngod@gmail.com>
2026-08-19 20:42:23 +0800

    发布 v29：Markdown 与 PDF 行程导出

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='minchao-v28';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./style.css?v=28','./data.js?v=28','./more-data.js?v=28','./app.js?v=28'];
+const CACHE='minchao-v29';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./style.css?v=29','./data.js?v=29','./more-data.js?v=29','./app.js?v=29'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
